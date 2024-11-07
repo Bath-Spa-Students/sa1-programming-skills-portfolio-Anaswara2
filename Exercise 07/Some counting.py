fec713b8766a2f16de3842dd005a987adc88648a
Anaswara2 <anaswara268@gmail.com>
2024-11-07 20:17:10 +0400

    Some counting.py

---
+++ Exercise 07/Some counting.py
@@ -0,0 +1,29 @@
+#Exercise 7: Some Counting
+""" Use your newly acquired knowledge of the for loop to complete the following
+tasks. Print all values to the console in each case. * Write a loop that counts
+up from 0 to 50 in increments of 1. * Write a loop that counts down from 50 to
+0 in decrements of 1. * Write a loop that counts up from 30 to 50 in increments
+of 1. * Write a loop that counts down from 50 to 10 in decrements of 2. * Write
+a loop that counts up from 100 to 200 in increments of 5. You may include all
+loops in a single project """
+
+#Write a loop that counts up from 0 to 50 in increments of 1.
+print("Displaying numbers from 0 to 50")
+for i in range(0,51,1):
+    print(i)
+
+#Write a loop that counts down from 50 to 0 in decrements of 1.
+for n in range(50,-1,-1):
+    print(n)
+
+#Write a loop that counts up from 30 to 50 in increments of 1.
+for s in range(30,51,1):
+    print(s)
+
+#Write a loop that counts down from 50 to 10 in decrements of 2.
+for a in range(50,9,-2):
+    print(a)
+
+#Write a loop that counts up from 100 to 200 in increments of 5.
+for h in range(100,201,5):
+    print(h)
