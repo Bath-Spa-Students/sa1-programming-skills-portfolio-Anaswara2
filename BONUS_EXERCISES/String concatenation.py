b8bfaf12e90eb6a7f1afc6a5e28028bb95f9b0bb
Anaswara2 <anaswara268@gmail.com>
2024-11-18 19:03:33 +0400

    String concatenation.py

---
+++ BONUS_EXERCISES/String concatenation.py
@@ -0,0 +1,11 @@
+#String concatenation.
+#Write three strings in different variables and print the output as one string.
+
+#code
+str1="Ana"
+str2="swa"
+str3="ra"
+#concatenating the strings
+str=str1+str2+str3
+#printing 3 strings as one string
+print(str)
