596ca14bd363f2ea286cd0248c52b8b00cb2567b
Anaswara2 <anaswara268@gmail.com>
2024-11-18 19:05:01 +0400

    Variables.py

---
+++ BONUS_EXERCISES/Variables.py
@@ -0,0 +1,9 @@
+#Variables
+#Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+#assigning "I am Anaswara" to the variable 'a'.
+a= "I am Anaswara"
+print(a)
+#assigning "I am studying in BSU" to the variable 'a'
+a= "I am studying in BSU"
+print(a)
