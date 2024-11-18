0d4b53c728e3c28a88dddb3ee8bdc22c9350374a
Anaswara2 <anaswara268@gmail.com>
2024-11-18 19:07:47 +0400

    Dictionaries.py

---
+++ BONUS_EXERCISES/Dictionaries.py
@@ -0,0 +1,16 @@
+#Use a dictionary to store information about a person you know.Store their first name,last name, age, and the city in which they live.
+
+"""
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary.
+"""
+
+#code
+#Creating a dictionary as asked in the question.
+dict={"first_name":"Anaswara",
+      "last_name":"Anish",
+      "age":18,
+      "city":"Dubai"}
+
+#printing the items
+print(dict.items())
