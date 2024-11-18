f42567d72239f7421c4ae3121ee1ccb7fe21fe62
Anaswara2 <anaswara268@gmail.com>
2024-11-18 19:06:20 +0400

    List -Names.py

---
+++ BONUS_EXERCISES/List -Names.py
@@ -0,0 +1,10 @@
+#List-Names
+#Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
+
+#code
+#Storing the names enclosed in square bracket to make a list
+Frnd_names=["Hadiya","Rinsha","Shadiya","Fatima","Safa"]
+#looping through the list 
+for x in Frnd_names:
+    #printing each item in the list
+    print(x)
