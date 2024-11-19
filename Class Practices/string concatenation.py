f40ad5ce5356995aba5e2ed5b8432ad35f557f27
Anaswara2 <anaswara268@gmail.com>
2024-11-19 13:51:41 +0400

    string concatenation.py

---
+++ Class Practices/string concatenation.py
@@ -0,0 +1,31 @@
+#String concatenation through variable
+msg='Do '+'the '+'things '+'that '+'matter'
+print(msg)
+
+#string concatenation through print statement
+print("Bathspa University "+"Academic centre "+"RAK campus")
+
+#string concatenation through diff variable
+a='Bathspa'
+b='university'
+c=a+" "+b
+print(c)
+
+#String concatenation through variable
+msg='Anaswara'+'Anish'
+print(msg)
+
+#string concatenation through print statement
+print("Anaswara "+"Anish")
+
+#string concatenation through diff variable
+a='Anaswara'
+b='Anish'
+c=a+" "+b
+print(c)
+
+a='Today'
+b='is'
+c='Thursday'
+d=a+" "+b+" "+c
+print(d)
