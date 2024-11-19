94b36f4c24558a422529d2e178c75c049a69b4bc
Anaswara2 <anaswara268@gmail.com>
2024-11-19 13:36:56 +0400

    functions.py

---
+++ Class Practices/functions.py
@@ -0,0 +1,13 @@
+#Functions
+
+def message(): #function header
+  msg="Anaswara" #local variable
+  print(msg) 
+  
+def message2(): 
+  msg="Anu"
+  print(msg)
+
+#function call
+message()
+message2()
