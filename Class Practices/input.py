cb17dbbdcebd13aff0acc3542c4df1e8e0b070e0
Anaswara2 <anaswara268@gmail.com>
2024-11-19 13:38:04 +0400

    input.py

---
+++ Class Practices/input.py
@@ -0,0 +1,15 @@
+#To get input from user
+first_name=input("Enter your name: ")
+print(first_name)
+
+middle_name=input("Enter your middle name: ")
+print(middle_name)
+
+Last_name=input("Enter your last name: ")
+print(Last_name)
+
+Age=int(input("Enter your age: "))
+print(Age)
+
+Place=input("Enter your place: ")
+print(Place)
