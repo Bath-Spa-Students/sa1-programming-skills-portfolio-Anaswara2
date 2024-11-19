0103f5a9c84b2cb31a33cacf1f149087d30ffe08
Anaswara2 <anaswara268@gmail.com>
2024-11-19 13:36:23 +0400

    end.py

---
+++ Class Practices/end.py
@@ -0,0 +1,9 @@
+#End delimiter
+
+name="Anaswara Anish"
+print(name,end=' ') #end delimiter stops the program from going to the next line
+print("NEW LINE")
+
+#/n and /t are known as escape sequences
+print("\n this line will print on the next line")
+print("\t this is tabbed in")
