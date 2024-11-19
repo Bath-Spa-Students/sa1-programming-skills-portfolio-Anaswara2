6637f5d29f0550c9915e1ed7f486dab462578a06
Anaswara2 <anaswara268@gmail.com>
2024-11-19 13:38:32 +0400

    list.py

---
+++ Class Practices/list.py
@@ -0,0 +1,19 @@
+#postive indexing
+num=[5,3,4,6,7,8]
+print(num[4])
+
+#negative indexing
+num=[7,8,3,5,6,4]
+print(num[-3])
+
+#finding length of list
+num=[5,6,8,3,3,4]
+print("The number of elements=",len(num))
+
+#finding index
+num=[5,6,7,0,2,3,1,4]
+print(num.index(3))
+
+#slicing
+num=[5,6,9,7,8,0]
+print(num.sort())
