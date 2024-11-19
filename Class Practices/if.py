2fe5d0f717eeb618dead93b99f908074e0bce6a8
Anaswara2 <anaswara268@gmail.com>
2024-11-19 13:37:27 +0400

    if.py

---
+++ Class Practices/if.py
@@ -0,0 +1,8 @@
+#if conditional statement.
+
+num1=700
+num2=2300
+if num1>num2: #checks whether the condition is true.
+    print("num2 is greater than num1")
+else: #prints the else statement if the condition returns false.
+    print("num1 is greater than num2")
